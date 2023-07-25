77fd7cd59e97b48377c2c05647cf472362c6eadb
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-07-25 12:41:42 +0530

    added some features

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 export default function App() {
   var [likeCounter, newCounter] = useState(0);
+  var [oldchange, newchange] = useState("");
 
   function updateCounter() {
     newCounter(likeCounter + 1);
@@ -12,11 +13,18 @@ export default function App() {
     updateCounter();
   }
 
+  function changeinput(event) {
+    newchange(event.target.value);
+    console.log({ oldchange });
+  }
+
   return (
     <div className="App">
       <h1>Hello World</h1>
       <h2>hello </h2>
       <button onClick={display}>click me</button> {likeCounter}
+      <input onChange={changeinput} />
+      <p>{oldchange}</p>
     </div>
   );
 }
